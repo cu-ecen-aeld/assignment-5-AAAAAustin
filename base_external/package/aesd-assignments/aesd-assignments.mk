3d5001275ed9bbea705804aaac46bf31e468a0ab
auspauld <auspauld@Ubuntu.myguest.virtualbox.org>
2024-10-05 17:00:33 -0600

    Testing new assignment3 makefile changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '3b70e026adf170b7bc58ed6ebffaf63dd041b5c1'
+AESD_ASSIGNMENTS_VERSION = 'bc8132936d8323ab2edc93d80fe7793c26e1df89'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
