2a653dcb1974c674845759073632258eda21b340
auspauld <auspauld@Ubuntu.myguest.virtualbox.org>
2024-10-06 16:28:55 -0600

    trying to fix aarch64 not found

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'bc8132936d8323ab2edc93d80fe7793c26e1df89'
+AESD_ASSIGNMENTS_VERSION = '070f01bf721f70732313e58868ee55fe1fdda29f'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
