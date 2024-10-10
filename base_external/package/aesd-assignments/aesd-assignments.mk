64d9aeafc63a7aef6355334f89e74e75304896e5
auspauld <auspauld@Ubuntu.myguest.virtualbox.org>
2024-10-09 19:46:18 -0600

    Still troubleshooting cc not found error

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '070f01bf721f70732313e58868ee55fe1fdda29f'
+AESD_ASSIGNMENTS_VERSION = 'f96f743fab01d30e7f1132fb8ee2290084041f92'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
